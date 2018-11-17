711e75fcf811be43c052fd98b7babafac11400f9
schmalzatop <spencercschmalz@gmail.com>
2018-11-16 19:35:18 -0500

    read image no conversion to .pgm

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <iostream>
 #include <stdlib.h> 
+#include <fstream>
+
 
 
 int main(int argc, char *argv[])
@@ -10,11 +12,27 @@ int main(int argc, char *argv[])
         std::cout << "wrong format! should be 'a.exe Image  Vertical_Size  Horizontal_Size'" << std::endl;
     else 
     { 
-        std::string img = argv[1];
+        std::string imgFile = argv[1];
         std::string vSize = argv[2];
         std::string hSize = argv[3];
+
+        //verify the read in of the image
+        std::ifstream img(imgFile);
+        if(!img.is_open())
+        {
+            std::cout << "The image could not be opened" << std::endl;
+        }
+
+        //convert image to .pgm
+
+
+
+        //determine if an enlarge or a shrink
         
-   }
+
+
+        img.close();
+    }
 
 	return 0;
 }
