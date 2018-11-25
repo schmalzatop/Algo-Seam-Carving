059a2ea4d638913130fade6343a0e9bda47addee
schmalzatop <spencercschmalz@gmail.com>
2018-11-25 15:33:20 -0500

    class for image working - base class

--- main.cpp
+++ main.cpp
@@ -8,45 +8,83 @@
 #include <fstream>
 #include <sstream>
 
-void process(std::string file)
+struct image
 {
-    std::ifstream image;
-    image.open(file);
-    std::stringstream ss;
-    std::string input = "";
-    int w = 0;
-    int h = 0;
-    //check for 'P2'
-    getline(image, input);
-    if(input.compare("P2") != 0)
+    //Member Variables
+    int w;
+    int h;
+    int ** imgarr;
+    //Deafult Constructor
+    image() {};
+    //File Name Constructor
+    image(std::string file)
     {
-        std::cerr << "Version Error: Not 'P2'" << std::endl;
-    }
-    
-    //ignore comment
-    getline(image, input);
-
-    //get size
-    ss << image.rdbuf();
-    ss >> h >> w;
-    
-    //create array
-    int arr[w][h];
-    
-    //read in values
-    for(int row = 0; row < w; row++)
-    {
-        for(int col = 0; col < h; col++)
+        std::ifstream image;
+        image.open(file);
+        std::stringstream ss;
+        std::string input = "";
+        //check for 'P2'
+        getline(image, input);
+        if(input.compare("P2") != 0)
         {
-            ss >> arr[row][col];
+            std::cerr << "Version Error: Not 'P2'" << std::endl;
+        }
+        
+        //ignore comment
+        getline(image, input);
+
+        //get size
+        ss << image.rdbuf();
+        ss >> w >> h;
+        imgarr = new int * [w];
+        for(int i = 0; i < w; i++)
+        {
+            imgarr[i] = new int[h];
+        }
+        //create array
+        int arr[w][h];
+        
+        //read in values
+        for(int row = 0; row < w; row++)
+        {
+            for(int col = 0; col < h; col++)
+            {
+                ss >> arr[row][col];
+            }
+        }
+
+        for(int i = 0; i < w; i++)
+        {
+            for(int o = 0; o < h; o++)
+            {
+                imgarr[i][o] = arr[i][o];
+            }
+        }
+
+        image.close();
+    }
+    //Class Functions
+    //print the 2d array
+    void printArr()
+    {
+        std::cout << "w: " << w << " | h: " << h << std::endl;
+        for(int row = 0; row < w; row++)
+        {
+            for(int col = 0; col < h; col++)
+            {
+                //std::cout << *(imgarr + row * h + col);
+                int num = imgarr[row][col];
+                std::cout << num << " ";
+            }
+            std::cout << "" << std::endl;
         }
     }
-
-    image.close();
-
-
-
-}
+    //create energy matrix
+    void energy()
+    {
+        
+    }
+};
 
 int main(int argc, char *argv[])
 {
@@ -63,7 +101,8 @@ int main(int argc, char *argv[])
     //check the file type
         if(imgFile.substr(imgFile.find_last_of(".") + 1) == "pgm")
         {
-            process(imgFile);
+            image i(imgFile);
+            
         }
         else
         {
