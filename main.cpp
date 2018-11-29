4638dddb70e46ee61d82c5bae8402dfac749186a
schmalzatop <spencercschmalz@gmail.com>
2018-11-29 08:20:53 -0500

    reqorked

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ struct image
     int h;
     int ** imgarr;
     int ** EP;
-    int** LCE;
+    int ** LCE;
     std::string gs;
 
     //Deafult Constructor
@@ -83,74 +83,181 @@ struct image
 
     //Class Functions
 
-    //create energy array
-    void energy()
+    void energy() //create energy array
     {
-        if(EP != NULL) 
+        if(EP != NULL) //if the array is already there destroy and re-make
         {
-        for(int i = 0; i < (w - 1); i++)
-        {
-            delete [] EP[i];
-        }
-        delete [] EP;
-        }
-
-        //create energy array
-        EP = new int * [h];
-        for(int i = 0; i < h; i++)
-        {
-            EP[i] = new int[w];
+            for(int x = 0; x < w; x++)
+            {
+                delete [] EP[x];
+            }
+            delete [] EP;
         }
 
-        //find energy values for each pixel
+        EP = new int *[h]; //create the energy array
+        for(int x = 0; x < h; x++)
+        {
+            EP[x] = new int[w];
+        }
+
         for(int y = 0; y < h; y++)
         {
             for(int x = 0; x < w; x++)
             {
-                int pixel = imgarr[y][x];   //pixel in question
-                int u, d, l, r;     //u - up; d - down; l - left; r - right;
-                //handle left side case
-                if(x == 0)
-                {
-                    l = imgarr[y][x];
-                }
-                else
-                {
-                    l = imgarr[y][x - 1];
-                }
-                //handle right side case
-                if(x == (w-1))
-                {
-                    r = imgarr[y][x];
-                }
-                else
-                {
-                    r = imgarr[y][x + 1];
-                }
-                //handle top case
-                if(y == 0)
-                {
-                    u = imgarr[y][x];
-                }
-                else
-                {
-                    u = imgarr[y - 1][x];
-                }
-                //handle bottom case
-                if(y == (h - 1))
-                {
-                    d = imgarr[y][x];
-                }
-                else
-                {
-                    d = imgarr[y + 1][x];
-                }
-                
-                //calc energy lvl and insert
-                EP[y][x] = (abs(pixel - l)+abs(pixel - r)+abs(pixel - u)+abs(pixel - d));
-                //std::cout << abs(pixel - l) << " + " << abs(pixel - r) << " + " << abs(pixel - u) << " + " << abs(pixel - d) << " = " << std::endl;
+                int pixel = imgarr[y][x]; //pixel to work on
+                int up, down, right, left; //surrounding pixels in question
+
+                if(x == 0)                      //LEFT: boundry case 
+                { left = imgarr[y][x]; }
+                else                            //LEFT: general case
+                { left = imgarr[y][x - 1]; }
+                if(x == (w - 1))                //RIGHT: boundry case
+                { right = imgarr[y][x]; }
+                else                            //RIGHT: general case
+                { right = imgarr[y][x + 1]; }
+                if(y == 0)                      //UP: boundry case
+                { up = imgarr[y][x]; }
+                else                            //UP: general case
+                { up = imgarr[y - 1][x]; }
+                if(y == (h - 1))                //DOWN: boundry case
+                { down = imgarr[y][x]; }
+                else                            //DOWN: general case
+                { down = imgarr[y + 1][x]; }
+
+                EP[y][x] = abs(pixel - left) + abs(pixel - right) + abs(pixel - up) + abs(pixel - down);
             }
-        } 
+        }
+    }
+
+    void orient() //function to rotate the array for carving 
+    {
+        
+    }
+
+    void lcearr() //creates the Least Cumlation Energy Array - for carving or adding
+    {
+        if(LCE != NULL) //if the array is already there destroy and re-make
+        {
+            for(int x = 0; x < w; x++)
+            {
+                delete [] LCE[x];
+            }
+            delete [] LCE;
+        }
+
+        LCE = new int *[h]; //create the energy array
+        for(int x = 0; x < h; x++)
+        {
+            LCE[x] = new int[w];
+        }
+
+        for(int y = 0; y < h; y++)
+        {
+            for(int x = 0; x < w; x++)
+            {
+                if(y == 0) //base case - top row 
+                { LCE[y][x] = EP[y][x]; }
+                else
+                {
+                    int val;
+                    if(x == 0) //cases: left, up
+                    { val = std::min(LCE[y - 1][x - 1], LCE[y - 1][x]); }
+                    else if(x == (w - 1)) //cases: up, right
+                    { val = std::min(LCE[y - 1][x], LCE[y - 1][x + 1]); }
+                    else //all cases: left, up, right
+                    { val = std::min(std::min(LCE[y - 1][x - 1], LCE[y -1][x]), LCE[y - 1][x + 1]); }
+                    LCE[y][x] = EP[y][x] + val;
+                }
+            }
+        }
+    }
+
+    void carve() //seam carving function
+    {
+        int start = 0;
+        for(int x = 0; x < w; x++) //find the smallest number in the bottom row
+        {
+            if(LCE[h - 1][x] < LCE[h - 1][start])
+            { start = x; }
+        }
+        imgarr[h - 1][start] = -1; //set the smallest value to -1 - to track the seam
+        
+        for(int x = 1; x < h; x++)
+        {
+            int height = h - 1 - x; //track the row from bottom up
+            int ls = start - 1;      //track the left col
+            int as = start;          //track the above col
+            int rs = start + 1;      //tracl the right col
+
+            if(start == 0)   //handle the case where there is no valid left
+            { ls = start; }
+            if(start == (w - 1)) //handle the case where there is no valid right
+            { rs = start; }
+            
+            //find the lowest value to go to
+            int go = std::min(std::min(LCE[height][ls], LCE[height][as]), LCE[height][rs]);
+            if(LCE[height][ls] == go)
+            { start = ls; }
+            else if(LCE[height][as] == go)
+            { start = as; }
+            else if(LCE[height][rs] == go)
+            { start = rs; }
+
+            imgarr[height][start] = -1;
+        }
+
+        /*//save the remaining parts of the image
+        std::vector<int> save;
+        for(int y = 0; y < h; y++)
+        {
+            for(int x = 0; x < w; x++)
+            {
+                if(imgarr[y][x] != -1)
+                { save.push_back(imgarr[y][x]); }
+            }
+        }
+
+        //delete the image array 
+        for(int x = 0; x < w; x++)
+        {
+            delete [] imgarr[x];
+        }
+        delete [] imgarr;
+
+        imgarr = new int * [h];
+        for(int x = 0; x < h; x++)
+        { imgarr[x] = new int[w]; }
+
+        int hold = 0;
+        for(int y = 0; y < h; y++)
+        {
+            for(int x = 0; x < w; x++)
+            {
+                imgarr[y][x] = save[hold];
+                ++hold;
+            }
+        }*/
+    }
+
+    void shrink(int hor, int vert)
+    {
+        //carve the vertical seams
+        for(int v = 0; v < vert; v++)
+        {
+            lcearr();
+            carve(); 
+            //printEArr();
+            //printLCEArr();
+            //printArr();
+        }
+        
+        orient();
+        std::cout << "-------------------" << std::endl;
+        //carve the horizontal seams
+        for(int h = 0; h < hor; h++)
+        {
+            
+        }
     }
 
     //print the 2d array - imgarr[vertical - h ][horizaotnal - w]
@@ -200,250 +307,6 @@ struct image
             std::cout << "" << std::endl;
         }
     }
-
-    //creates the Least Cumlation Energy Array
-    void larr()
-    {
-        //create the Least Cummlation Energy Array
-        //int** LCE;
-        LCE = new int * [h];
-        for(int i = 0; i < h; i++)
-        {
-            LCE[i] = new int[w];
-        }
-        //fill the LCE Array  - if above row has equal options will choose leftmost option
-        for(int y = 0; y < h; y++)
-        {
-            for(int x = 0; x < w; x++)
-            {
-                if(y == 0) //top row condition
-                {
-                    LCE[y][x] = EP[y][x];
-                }
-                else if(x == 0) //left most condtion 
-                {
-                    if(LCE[y - 1][x + 1] < LCE[y - 1][x]) //right < above
-                    {
-                        LCE[y][x] = LCE[y - 1][x + 1] + EP[y][x];
-                    }
-                    else //above < right
-                    {
-                        LCE[y][x] = LCE[y - 1][x] + EP[y][x];
-                    }
-                }
-                else if(x == (h - 1)) //right most condition
-                {
-                    if(LCE[y - 1][x] < LCE[y - 1][x - 1]) //above < left
-                    {
-                        LCE[y][x] = LCE[y - 1][x] + EP[y][x];
-                    }
-                    else //left < above
-                    {
-                        LCE[y][x] = LCE[y - 1][x - 1] + EP[y][x];
-                    }
-                }
-                else //compare right, above, & left condition
-                {
-                    int smallest = LCE[y - 1][x - 1];
-                    if (smallest > LCE[y - 1][x]) //if left is bigger than above set to above
-                    {
-                        smallest = LCE[y - 1][x];
-                    }
-                    if(smallest > LCE[y - 1][x + 1]) // left or above is bigger than right set to right
-                    {
-                        smallest = LCE[y - 1][x + 1];
-                    }
-                    LCE[y][x] = smallest + EP[y][x];
-                }
-                //std::cout << "Row: " << y << "Col: " << x << std::endl;
-            }
-        }
-    }
-
-    void orient()
-    {
-        //flip the values
-        int ** arrimg = imgarr;
-        int x = w;
-        int y = h;
-        h = x;
-        w = y;
-        
-        //rebuild
-        imgarr = new int * [h];
-        for(int i = 0; i < h; i++)
-        {
-            imgarr[i] = new int[w];
-        }
-
-        for(int k = 0; k < h; k++)
-        {
-            for(int j = 0; j < w; j++)
-            {
-                imgarr[k][j] = arrimg[w - 1 - j][k];
-            }
-        }
-
-        energy();
-        
-        //clean up
-        for(int d = 0; d < h; d++)
-        {
-            delete [] arrimg[d];
-        }
-        delete [] arrimg;
-    }
-
-    void carve()
-    {
-        int min = 0;
-        for(int i = 0; i < w; i++)  //find the col with the smallest value in bottom row - if tie leftmost is chosen
-        {
-            if(LCE[h - 1][i] < LCE[h - 1][min])
-            {
-                min = i;
-            }
-        }
-        imgarr[h - 1][min] = -1;      //mark spots to remove with -1
-        int ncol = min;
-        for(int c = 0; c < h; c++)
-        {
-            int lvl = h - 1 - c;
-            int above = ncol;
-            int left = ncol - 1;
-            int right = ncol + 1;
-
-            //handle the edge cases
-            if(left <= 0)
-            {
-                left = above;
-            }
-            if(right >= h - 1)
-            {
-                right = above;
-            }
-
-            //pick the next position
-            int smallest = LCE[lvl][left];
-            if(smallest > LCE[lvl][above]) //if left is bigger than above set to above
-            {
-                smallest = LCE[lvl][above];
-            }
-            if(smallest > LCE[lvl][right]) //if left or above is bigger then right set to right
-            {
-                smallest = LCE[lvl][right];
-            }
-            if(smallest == LCE[lvl][left])
-            {
-                ncol = left;
-            }
-            else if(smallest == LCE[lvl][above])
-            {
-                ncol = above;
-            }
-            else
-            {
-                ncol = right;
-            }
-
-            imgarr[lvl][ncol] = -1;
-        }
-        std::vector<int> temp;
-        int f = w;
-        --w;
-        printArr();
-        for(int j = 0; j < h; j++)
-        {
-            for(int k = 0; k < f; k++)
-            {
-                if(imgarr[j][k] != -1)
-                {
-                    temp.push_back(imgarr[j][k]);
-                }
-            }
-        }
-        
-        //delete the orginal array to remove the -1 and shrink
-        for(int d = 0; d < w; d++)
-        {
-            delete [] imgarr[d];
-        }
-        delete [] imgarr;
-
-        //recreate the orginal array at smaller size
-        imgarr = new int * [h];
-        for(int i = 0; i < h; i++)
-        {
-            imgarr[i] = new int[w];
-        }
-
-        //fill the array
-        int spot = 0;
-        for(int i = 0; i < h; i++)
-        {
-            for(int o = 0; o < w; o++)
-            {
-                imgarr[i][o] = temp[spot];
-                spot++;
-            }
-        }
-        temp.clear();
-    }
-
-    void shrink(int hor, int vert)
-    {
-        //carve the vertical seams
-        for(int v = 0; v < vert; v++)
-        {
-            larr();
-            carve();
-            energy();  
-        }
-        
-        orient();
-    
-        //carve the horizontal seams
-        for(int h = 0; h < hor; h++)
-        {
-            larr();
-            carve();
-            printArr();
-            //energy();
-        }
-    }
-
-    void enlarge(int hor, int vert)
-    {
-        std::cout << "Feature to be Added" << std::endl;
-        //create the Least Cummlation Energy Array
-        larr();
-        //Find seam - while loop for amount of seams - Horizontal - Add Seams
-
-        //Find seam - while loop for amount of seams - Vertical - Add Seams
-    }
-
-    void remove()
-    {
-        std::cout << "Feature to be Added" << std::endl;
-    }
-
-    //writes processed img to file with modified name
-    void processed(std::string name) 
-    {
-        int here = name.find_last_of('.');
-        std::string output = name.substr(0, here);
-        output += "_processed.pgm";
-        std::ofstream file(output);
-        file << "P2" << '\n' << w << ' ' << h << '\n' << gs << '\n';
-        for(int i = 0; i < h; i++) 
-        {
-            for (int j = 0; j < w; j++) 
-            {
-                file << imgarr[j][i] << ' ';
-            }
-        }
-        file.close();
-    }
 };
 
 int main(int argc, char *argv[])
@@ -475,13 +338,13 @@ int main(int argc, char *argv[])
             else if(chng == "E")
             {
                 //FIX-ME    add support to enlarge images
-                i.enlarge(hSeams, vSeams);
+                //i.enlarge(hSeams, vSeams);
                 //i.processed();
             }
             else if(chng == "R")
             {
                 //FIX-ME    add suuport to remove items
-                i.remove();
+                //i.remove();
                 //i.processed();
             }
             else
